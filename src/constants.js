34f9edf41e348fa535c6403f3e2600f29569e746
Geli Yang <gyang5@villanova.edu>
2022-09-05 21:46:46 -0400

    change icon and font color for title

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = "token";
-export const BASE_URL = "https://around-dot-around-179500.uc.r.appspot.com";
+export const BASE_URL = "https://concise-base-357914.ue.r.appspot.com";
 
 export const SEARCH_KEY = {
   all: 0,
